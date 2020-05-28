c15242b880777c1ad9ded9bfaf8829b376dbed4f
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-28 17:11:24 +0200

    footer added, more style

--- partials/templates/footer.php
+++ partials/templates/footer.php
@@ -1,3 +1,9 @@
+    <footer>
+        <div class="container">
+            <p class="h6 text-right">Made by Thang<br>28/05/2020</p>
+        </div>
+    </footer>
+    
     <script src="dist/script/main.js"></script>
     </body>
 </html> 
