14db7188443d08862acaaa18bffa5cec250f7cf4
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-28 15:15:10 +0200

    data.php completed

--- index.php
+++ index.php
@@ -8,8 +8,31 @@
     <link rel="stylesheet" href="dist/css/main.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Rooms</h1>
+    </header>
 
+    <main>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Room number</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>100</td>
+                    <td class=".text-info">
+                        <a href="">View</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
 
     <!-- TEMPLATE-->
     <script id="entry-template" type="text/x-handlebars-template">
--- partials/data/data.php
+++ partials/data/data.php
@@ -1,2 +1,8 @@
 <?php
-include dirname(__DIR__) . '/data/data.php';
+
+include __DIR__ . '../../../env.php';
+
+$conn = new mysqli($server_name, $username, $password, $db_name);
+if ($conn && $conn->connect_error) {
+    die('Connection error');
+}
