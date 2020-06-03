309760e6491c397cd0f84f1370bd2568916a0c89
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-03 09:59:26 +0200

    bind_param on server-update

--- partials/update/server-update.php
+++ partials/update/server-update.php
@@ -10,11 +10,23 @@ $room_number = $_POST['room_number'];
 $beds = $_POST['beds'];
 $floor = $_POST['floor'];
 
+/*
 $sql = "UPDATE `stanze`
         SET `room_number` = $room_number,
             `beds` = $beds,
             `floor` = $floor
         WHERE `id` = $id_room";
+*/
+
+$sql = "UPDATE `stanze`
+        SET `room_number` = ?,
+            `beds` = ?,
+            `floor` = ?.
+        WHERE `id` = ?";
+
+$stmt = $conn-> prepare($sql);
+$stmt->bind_param('iiii', $room_number, $beds, $floor, $id_room);
+
 
 $result = $conn->query($sql);
 
